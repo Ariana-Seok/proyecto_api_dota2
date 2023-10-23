ec7456f5ccd58a72db2e7011b5a897be9db8ca17
Daniela Fernandez Laura <dfernandezl@fi.uba.ar>
2023-10-23 09:36:15 -0300

    Carga de archivos y programas

--- app/routes/heroe.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = app => {
-    const heroes = require("../controllers/heroe.controller.js");
-
-    const router = require("express").Router();
-
-    // Crea un nuevo Heroe
-    router.post("/", heroes.create);
-    app.use("/api/heroes", router);
-};
